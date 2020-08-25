b456e0ebe15562b34b7e87f2ffc94d80c2f56d7e
Daniil Syurmachenko <vxlkxvik@yandex.ru>
2020-08-26 00:05:51 +0300

    Add login via discord

--- web/main.py
+++ web/main.py
@@ -1,12 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for, abort
+
+from web.utilities import *
 
 
 app = Flask("Ice Cube")
+app.secret_key = CLIENT_SECRET
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    token = session.get("oauth2_token")
+
+    if token:
+        discord = make_session(token=session.get("oauth2_token"))
+        user = get_user(discord)
+
+        data = {
+            "auth": True,
+            "username": user["username"]
+        }
+    else:
+        data = {"auth": False}
+
+    return render_template("index.html", **data)
+
+
+@app.route("/invite_bot")
+def invite_bot():
+    return redirect("https://discord.com/oauth2/authorize?client_id=637682828284395530&scope=bot&permissions=8")
+
+
+@app.route("/login")
+def login():
+    discord = make_session(scope="identify guilds")
+    authorization_url, state = discord.authorization_url(AUTHORIZATION_BASE_URL)
+
+    session["oauth2_state"] = state
+
+    return redirect(authorization_url)
+
+
+@app.route("/login/callback", methods=["get"])
+def login_callback():
+    if request.values.get("error"):
+        return request.values["error"]
+
+    discord = make_session(state=session.get("oauth2_state"))
+    token = discord.fetch_token(
+        TOKEN_URL,
+        client_secret=CLIENT_SECRET,
+        authorization_response=request.url)
+
+    session["oauth2_token"] = token
+
+    return redirect(url_for(".index"))
+
+
+@app.route("/logout")
+def logout():
+    if "oauth2_token" in session.keys():
+        del session["oauth2_token"]
+    else:
+        abort(401)
+
+    return redirect(url_for(".index"))
 
 
 if __name__ == '__main__':
---
+++ web/utilities.py
@@ -0,0 +1,36 @@
+from os import environ
+
+from flask import session
+from requests_oauthlib import OAuth2Session
+
+
+API_ENDPOINT = "https://discord.com/api/v6"
+AUTHORIZATION_BASE_URL = API_ENDPOINT + "/oauth2/authorize"
+TOKEN_URL = API_ENDPOINT + "/oauth2/token"
+
+CLIENT_ID = environ.get("CLIENT_ID")
+CLIENT_SECRET = environ.get("CLIENT_SECRET")
+REDIRECT_URI = f"{environ.get('REDIRECT_URI')}/login/callback"
+
+
+def token_updater(token):
+    session["oauth2_token"] = token
+
+
+def make_session(token=None, state=None, scope=None):
+    return OAuth2Session(
+        client_id=CLIENT_ID,
+        token=token,
+        state=state,
+        scope=scope,
+        redirect_uri=REDIRECT_URI,
+        auto_refresh_kwargs={
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        },
+        auto_refresh_url=TOKEN_URL,
+        token_updater=token_updater)
+
+
+def get_user(oauth2_session):
+    return oauth2_session.get(API_ENDPOINT + "/users/@me").json()
